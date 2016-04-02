cc7fbf3a9d3f49f6029ee872bcbcf279201dfb99
nodes11 <desmarks11@gmail.com>
2016-04-02 15:59:16 -0700

    Added name functionality and cleaned up the UI a little. Channels are next.

--- client.go
+++ client.go
@@ -5,11 +5,22 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
 )
+
+var name string
+var reader = bufio.NewReader(os.Stdin)
+
+func readName() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Name: ")
+	message, _ := reader.ReadString('\n')
+	return message[0: len(message) - 1]
+}
 
 func read() string {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf(">")
+	fmt.Printf(name)
 	message, _ := reader.ReadString('\n')
 	return message
 }
@@ -17,11 +28,26 @@ func read() string {
 func getMessages(c net.Conn) {
 	for {
 		nmessg, _ := bufio.NewReader(c).ReadString('\n')
-		fmt.Printf(string('\n') + nmessg + ">")
+		fmt.Printf(string('\n') + nmessg + name)
 	}
 }
 
+func clearScreen(){
+	c := exec.Command("clear")
+	c.Stdout = os.Stdout
+	c.Run()
+
+	fmt.Printf("Chat Room:\n")
+	fmt.Printf("--------------------------------------------------------------------------------\n")
+}
+
 func main() {
+	clearScreen();
+	name = readName()
+	clearScreen()
+
+	name += ": "
+
 	con, err := net.Dial("tcp", "127.0.0.1:5555")
 	if err != nil {
 		panic(err)
@@ -30,7 +56,8 @@ func main() {
 	for {
 		go getMessages(con)
 
-		messg := read()
+		messg := name
+		messg += read()
 		fmt.Fprintf(con, messg)
 	}
 }
--- server.go
+++ server.go
@@ -5,23 +5,65 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
 )
 
+var name string
+var messg string
+
+var reader = bufio.NewReader(os.Stdin)
+
+func readName() string {
+	reader = bufio.NewReader(os.Stdin)
+	fmt.Printf("Name: ")
+	message, _ := reader.ReadString('\n')
+	return message[0:len(message) - 1]
+}
+
 func read() string {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf(">")
+	reader = bufio.NewReader(os.Stdin)
+	fmt.Printf(name)
 	message, _ := reader.ReadString('\n')
 	return message
 }
 
 func getMessages(c net.Conn) {
 	for {
-		messg, _ := bufio.NewReader(c).ReadString('\n')
-		fmt.Printf(string('\n') + string(messg) + ">")
+		messg, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil{
+			break;
+		}
+
+		//scanner := bufio.NewScanner(os.Stdin)
+		//tempText := scanner.Text()
+
+		fmt.Printf(string('\n') + string(messg) + name)
 	}
+
+
+		fmt.Printf("Connection Closed")
+}
+
+func clearScreen(){
+	c := exec.Command("clear")
+	c.Stdout = os.Stdout
+	c.Run()
+
+	fmt.Printf("Chat Room:\n")
+	fmt.Printf("--------------------------------------------------------------------------------\n")
 }
 
 func main() {
+	//Get user name
+	clearScreen();
+	name = readName()
+	clearScreen()
+
+
+
+	name += ": "
+
+	//Get a connection from a client
 	listen, err := net.Listen("tcp", ":5555")
 	if err != nil {
 		panic(err)
@@ -29,10 +71,15 @@ func main() {
 
 	con, _ := listen.Accept()
 
+	//CHAT!!!
 	for {
+
 		go getMessages(con)
 
-		messg := read()
+		messg = name
+
+		messg += string(read())
+
 		con.Write([]byte(messg))
 	}
 }
